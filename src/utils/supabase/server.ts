7c3b99e48cd0da37b69cd722541c6790392e7fca
Icaro Wuandson <icarowuandson1@gmail.com>
2025-01-14 10:24:59 -0300

    corrigir server.ts

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelos valores do seu projeto Supabase
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Verifique se as variáveis de ambiente estão definidas
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-
+// Verifique se as variáveis são válidas
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('As variáveis de ambiente do Supabase não estão configuradas corretamente.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
